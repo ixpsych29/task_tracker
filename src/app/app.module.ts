c42883d7b2215ea9d0b9ad4d0e99e7b1d3af0c65
Abdullah <abdullahbinrafique945@gmail.com>
2022-06-01 23:46:10 +0500

    Updating things to more advance level

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { ButtonComponent } from './MyComponents/button/button.component';
+import { TasksComponent } from './MyComponents/tasks/tasks.component';
+import { TaskItemComponent } from './MyComponents/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
