4ada214070782d8dc9b448dce663e86b14429d11
Abdullah <abdullahbinrafique945@gmail.com>
2022-06-02 12:43:22 +0500

    delete

--- src/app/MyComponents/tasks/tasks.component.ts
+++ src/app/MyComponents/tasks/tasks.component.ts
@@ -17,4 +17,10 @@ export class TasksComponent implements OnInit {
     this.taskService.getTasks().subscribe((tasks: Task[]) => this.tasks = tasks);
   }
 
+  deleteTask(task: Task){
+    this.taskService
+    .deleteTask(task)
+    .subscribe(
+      () => this.tasks = this.tasks.filter(t => t.id !== task.id));
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import {Task} from '../Task';
-import {Observable, of} from 'rxjs'
-import {TASKS} from '../mock-tasks'
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks  = of(TASKS);
-    return tasks;
+    console.log(this.http);
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    console.log(task.id);
+    const url = `${this.apiUrl}/${task.id}`; //  apiurl+'/'+task.id;
+    return this.http.delete<Task> (url);
   }
 }
