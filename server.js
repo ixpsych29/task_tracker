90b8d254e7503e31f8d35870203af92e0ff2ee8f
Abdullah <abdullahbinrafique945@gmail.com>
2022-06-08 01:04:44 +0500

    port changes

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/*', function(req,res)
 
 {res.sendFile(path.join(__dirname+'/dist/task-tracker","version": "0.0.0",/index.html'));});
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
