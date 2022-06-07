2c3fba137229a9eec387b5e5db163acc2b867932
Abdullah <abdullahbinrafique945@gmail.com>
2022-06-08 01:37:07 +0500

    Minor changes

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/task-tracker","version": "0.0.0",'));
-app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/task-tracker","version": "0.0.0",/index.html'));});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/task-tracker'));
 
-app.listen(process.env.PORT || 4200);
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/task-tracker/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
