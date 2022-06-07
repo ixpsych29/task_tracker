6fa91109f3e41cec7454666f0ca96e21c412be03
Abdullah <abdullahbinrafique945@gmail.com>
2022-06-08 01:44:25 +0500

    changing the db link

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -15,7 +15,7 @@ const httpOptions = {
 })
 export class TaskService {
 
-  private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl = 'https://json-file-db.herokuapp.com/tasks';
 
   constructor(private http: HttpClient) { }
 
